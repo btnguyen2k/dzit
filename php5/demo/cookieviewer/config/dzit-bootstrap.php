23a4ce828e866b2906cee5c9c12660d47c156d72
btnguyen2k <btnguyen2k@f6278f3e-2d47-0410-a02b-4f11275fb890>
2011-07-27 08:18:12 +0000

    * Fixed warning messages

--- php5/demo/cookieviewer/config/dzit-bootstrap.php
+++ php5/demo/cookieviewer/config/dzit-bootstrap.php
@@ -3,43 +3,29 @@ defined('DZIT_INCLUDE_KEY') || die('No direct access allowed!');
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 /**
  * Dzit's core bootstrap file.
+ * What this bootstrap does?
+ * - Load each application module's bootstrap file.
  */
 
-/*
- * Note: Change this if your "modules" directory is located at another location!
- */
-define('MODULES_DIR', '../modules');
-
-/* set up include path */
-$includePath = ini_get('include_path');
-if (($dh = @opendir(MODULES_DIR)) !== FALSE) {
-    while (($file = readdir($dh)) !== FALSE) {
-        if (is_dir(MODULES_DIR . "/$file") && $file != "." && $file != "..") {
-            $includePath .= PATH_SEPARATOR . MODULES_DIR . "/$file";
-        }
+if (defined('MODULES_DIR')) {
+    /* load module bootstraps */
+    $_BOOTSTRAPS = Array();
+    if (!defined("MODULE_BOOTSTRAP_FILE")) {
+        define('MODULE_BOOTSTRAP_FILE', 'bootstrap.php');
     }
-} else {
-    exit('Can not open MODULES_DIR!');
-}
-ini_set('include_path', $includePath);
-
-/* load module bootstraps */
-$_BOOTSTRAPS = Array();
-if ( !defined("MODULE_BOOTSTRAP_FILE") ) {
-    define('MODULE_BOOTSTRAP_FILE', 'bootstrap.php');
-}
-if (($dh = @opendir(MODULES_DIR)) !== FALSE) {
-    while (($file = readdir($dh)) !== FALSE) {
-        if (is_dir(MODULES_DIR . "/$file") && $file != "." && $file != "..") {
-            $bootstrap = MODULES_DIR . "/$file/".MODULE_BOOTSTRAP_FILE;
-            if (file_exists($bootstrap)) {
-                $_BOOTSTRAPS[] = $bootstrap;
+    if (($dh = @opendir(MODULES_DIR)) !== FALSE) {
+        while (($file = readdir($dh)) !== FALSE) {
+            if (is_dir(MODULES_DIR . "/$file") && $file != "." && $file != "..") {
+                $bootstrap = MODULES_DIR . "/$file/" . MODULE_BOOTSTRAP_FILE;
+                if (file_exists($bootstrap)) {
+                    $_BOOTSTRAPS[] = $bootstrap;
+                }
             }
         }
     }
-}
-//bootstrap files are included in order!
-sort($_BOOTSTRAPS);
-foreach ($_BOOTSTRAPS as $bootstrap) {
-    include_once $bootstrap;
+    //bootstrap files are included in order!
+    sort($_BOOTSTRAPS);
+    foreach ($_BOOTSTRAPS as $bootstrap) {
+        include_once $bootstrap;
+    }
 }
